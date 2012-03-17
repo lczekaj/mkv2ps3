f229ab24b1fe6c124f4f472d3b731fa11397c3d6
Lukasz Czekaj <lukasz@czekaj.us>
2012-03-17 14:55:10 +0100

    first version of my fork

--- mkv2ps3.sh
+++ mkv2ps3.sh
@@ -2,10 +2,11 @@
 # Script to convert mkv files to PS3 playable mp4
 # Coded by Werner Gillmer <werner.gillmer@gmail.com>
 # following psilo357 advice from https://bbs.archlinux.org/viewtopic.php?id=62298&p=2
+# Extended by Lukasz Czekaj <lukasz[at]czekaj.us>
 
 # Usage : mkv2ps3.sh movie.mkv
 #         mkv2ps3.sh /path/to/dir/with/mkv/files 
-# NOTE : Original mkv file get's deleted after conversion.
+# or	: mkv2ps3.sh movie.mkv /output_dir/
 
 # MP4Box http://gpac.wp.institut-telecom.fr/mp4box/
 # mkvtoolnix http://www.bunkus.org/videotools/mkvtoolnix/
@@ -13,18 +14,27 @@
 
 # Tested on Linux (Arch) and OS X(Lion)
 
+OUTPUT_DIR=$2
+
 function cleanup {
     echo -n "### Cleaning up..."
-    rm $1
+    # to delete original file after conversion just uncomment the line below
+    # rm $1
     rm *mkv.dts
     rm *mkv.aac
     rm *mkv.264
 
-    NEWNAME=`ls $1.mp4 | sed 's/mkv.mp4/mp4/'`
-    mv $1.mp4 $NEWNAME
-
+	if  [ -d $OUTPUT_DIR ]; then
+	    NEWNAME=`ls $OUTPUT_DIR$1.mp4 | sed 's/mkv.mp4/mp4/'`
+	    mv $OUTPUT_DIR$1.mp4 $NEWNAME
+	else
+	    NEWNAME=`ls $1.mp4 | sed 's/mkv.mp4/mp4/'`
+	    mv $1.mp4 $NEWNAME
+	fi
+	
     echo "done"
 }
+
 
 function mkv2ps3 {
     # Extracts the video stream
@@ -36,11 +46,17 @@ function mkv2ps3 {
     ffmpeg -i $1.dts -vcodec libfaac $1.aac
 
     # Merge everything to mp4
-    MP4Box -new $1.mp4 -add $1.264 -add $1.aac -fps 23.976
+	if [ -d $OUTPUT_DIR ]; then
+		    echo -e "### Creating .mp4 at $OUTPUT_DIR$1\r"
+	    MP4Box -new $OUTPUT_DIR$1.mp4 -add $1.264 -add $1.aac -fps 23.976
+	else
+	    MP4Box -new $1.mp4 -add $1.264 -add $1.aac -fps 23.976
+	fi
 
     cleanup $1 
 }
 
+echo -e "Output will be copied to: $OUTPUT_DIR\r"
 
 if [ -d $1 ]; then
     DIR=$1
